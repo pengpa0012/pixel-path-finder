2672833c3f2fe5b26f223333078fa53b440503d9
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-17 22:39:51 +0800

    added end pixel, pixel chase

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@
 // add path find algo
 
 let dir = {x: 250, y: 250}
+const endCoords = {x: 490, y: 10}
 
 function setup() {
   createCanvas(500, 500)
@@ -12,18 +13,35 @@ function setup() {
 
 // pixel ticker
 const timer = setInterval(() => {
-  const nextX = Math.random() * 2 > 1 ? 10 : -10
-  const nextY = Math.random() * 2 > 1 ? 10 : -10
-  dir.x += nextX
-  dir.y += nextY
+  if(dir.x < endCoords.x) {
+    dir.x += 10
+  }
+  if(dir.x > endCoords.x) {
+    dir.x -= 10
+  }
+  if(dir.y < endCoords.y) {
+    dir.y += 10
+  }
+  if(dir.y > endCoords.y) {
+    dir.y -= 10
+  }
+}, 100)
 
-}, 500)
+
+setInterval(() => {
+  const randomX = Math.floor(random(0, 490) / 10) * 10
+  const randomY = Math.floor(random(0, 490) / 10) * 10
+  endCoords.x = randomX 
+  endCoords.y = randomY
+}, 1000)
 
 function draw() {
   clear()
   fill(0)
   noStroke()
   rect(Math.min(Math.max(dir.x, 0), 500), Math.min(Math.max(dir.y, 0), 500), 10, 10)
+  fill(136, 8, 8)
+  rect(endCoords.x, endCoords.y, 10, 10)
 
   // create blocks
   if(mouseIsPressed) {
